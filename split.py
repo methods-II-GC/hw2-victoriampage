4ebc2ad42ff607eecfedcd5f5cbbae8498fc3f18
V <victoriampage@gmail.com>
2021-03-07 19:37:39 -0500

    Added description of program, copied in read_tags().

--- split.py
+++ split.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
-"""One-line description of the program goes here."""
+"""Read in data from a file and split the data into training, dev, and test sets."""
 
 
 import argparse
 
+from typing import Iterator, List
+
+
+def read_tags(path: str) -> Iterator[List[List[str]]]:
+    with open(path, "r") as source:
+        lines = []
+        for line in source:
+            line = line.rstrip()
+            if line:  # Line is contentful.
+                lines.append(line.split())
+            else:  # Line is blank.
+                yield lines.copy()
+                lines.clear()
+    # Just in case someone forgets to put a blank line at the end...
+    if lines:
+        yield lines
 
 def main(args: argparse.Namespace) -> None:
     # TODO: do the work here.
+
     ...
 
 
