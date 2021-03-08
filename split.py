f49a6861ee863815165edc0dfee11773d69584d5
V <victoriampage@gmail.com>
2021-03-07 21:22:36 -0500

    Made code nicer-looking with flake8/black

--- split.py
+++ split.py
@@ -23,13 +23,11 @@ def read_tags(path: str) -> Iterator[List[List[str]]]:
         yield lines
 
 
-# writes section of the data to a path
+# writes section of the data to a path file
 def write_tag(path: str, set):
     with open(path, "w") as file:
         for line in set:
-            #print(line)
-            #print(words)
-            for word in line: #found some weirdness at the end of each run, not sure what it is
+            for word in line:
                 if type(word) == list:
                     file.writelines(word)
                     file.writelines("\n")
@@ -39,11 +37,12 @@ def main(args: argparse.Namespace) -> None:
     # TODO: do the work here.
     corpus = list(read_tags(args.input))
 
-    #Add seed and randomize
+    # Add seed and randomize
     seed = args.seed
     random.seed(seed)
     random.shuffle(corpus)
 
+    # split data into 80-10-10
     eighty = int(len(corpus) * 0.8)
     ninety = int(len(corpus) * 0.9)
 
@@ -51,6 +50,7 @@ def main(args: argparse.Namespace) -> None:
     dev_data = corpus[eighty:ninety]
     test_data = corpus[ninety:]
 
+    # output split data to files
     write_tag(args.train, train_data)
     write_tag(args.dev, dev_data)
     write_tag(args.test, test_data)
@@ -68,8 +68,8 @@ if __name__ == "__main__":
 
     parser.add_argument("test", type=str)
 
-    parser.add_argument(
-        "--seed", type=int, required=True, help="Seed is required.")
+    # non-optional seeder for randomization
+    parser.add_argument("--seed", type=int, required=True, help="Seed is required.")
 
     # TODO: parse arguments and pass them to `main`.
     main(parser.parse_args())
